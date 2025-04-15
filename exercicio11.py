03c87a84816de39f43312d1418d2d11854836809
Samuel-millerr <samuel.m.soares10@aluno.senai.br>
2025-04-15 16:31:11 -0300

    Exercicios Estrutura de Repetição

--- exercicio11.py
+++ exercicio11.py
@@ -60,6 +60,7 @@ while escolha_menu_principal == 1:
                     print("\nA opção que você escolheu é inválida! Por gentileza tente novamente.")
                     escolha_usuario = int(input("Seu turno, escolha: \n[1] Atacar \n[2] Curar \n[3] Usar Itens \n > "))
 
+        # TOMADA DE DECISÃO DO USUÁRIO
         if escolha_usuario == 1:
             acao_ataque_usuario = ataque_usuario - defesa_inimigo
             if random.random() <= 0.1:
@@ -76,6 +77,13 @@ while escolha_menu_principal == 1:
             print("[3] Poção de Defesa (DEF +20 5 turnos)")
             print("[4] Poção Explosiva (dano direto de 60)")
             item = input("Escolha o item: ")
+
+            # TRATAMENTO DE ERRO CASO O USUÁRIO DIGITE ALGUMA OPÇÃO QUE NÃO ESTÁ DISPONÍVEL
+            while item not in [1, 2, 3, 4]:
+                print("\nA opção que você escolheu é inválida! Por gentileza tente novamente.")
+                print(("Seu turno, escolha: \n[1] Poção de Força (+ATQ por 2 turnos) \n[2] Poção de Cura (+100 HP) \n[3] Poção de Defesa (DEF +20 5 turnos) \n[4] Poção Explosiva (dano direto de 60) \n > "))
+                item = input("Escolha o item: ")
+
             if item not in item_usado_usuario:
                 if item == "1": ataque_usuario += 20; item_usado_usuario.append(item); print("Força aumentada em 20 pontos!")
                 elif item == "2": vida_usuario += 100; item_usado_usuario.append(item); print("Curado em 100 pontos de HP!")
@@ -96,22 +104,21 @@ while escolha_menu_principal == 1:
         elif escolha_inimigo == "curar":
             print("O inimigo se curou! ELe recuperou 20 HP")
             vida_inimigo = min(vida_inimigo+20, 1000)
-        elif escolha_inimigo == "usar_itens":
+        elif escolha_inimigo == "usar_itens" and len(item_usado_inimigo) < 5:
             print("O inimigo irá usar um item!")
             sleep(0.5)
             print(".")
             sleep(0.5)
             print(".")
-            sleep(0.5)
-            print(".")
             item = str(random.randint(1,4))
-            if item not in item_usado_inimigo:
+            if item not in item_usado_inimigo and len(item_usado_inimigo) < 5:
                 if item == "1": ataque_inimigo += 20; item_usado_inimigo.append(item); print("O inimigo escolheu a poção de Força. O ataque do inimigo é aumentado em 20 pontos!")
-                elif item == "2": vida_inimigo += 100; vida_inimigo = min(vida_inimigo, vida); item_usado_inimigo.append(item); print("O inimigo escolheu a poção de cura. O inimigo é curado em 100 pontos de HP!")
+                elif item == "2": vida_inimigo += 100; item_usado_inimigo.append(item); print("O inimigo escolheu a poção de cura. O inimigo é curado em 100 pontos de HP!")
                 elif item == "3": defesa_inimigo += 20; item_usado_inimigo.append(item); print("O inimigo escolheu a poção de defesa. A defesa do inimigo é aumentada em 20 pontos!")
                 elif item == "4": vida_usuario -= 60; item_usado_inimigo.append(item); print("Explosão em VOCÊ! Você perdeu 60 pontos de HP")
             else:
                 print("O inimigo não tem mais itens!")
+
         if vida_usuario <= 0 or vida_inimigo <= 0:
             break
 
